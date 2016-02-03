df8455a0babcce5bbc6194ffb55ed9655948a263
wmitch96 <w.mitch96@gmail.com>
2016-02-03 12:58:15 +0000

    Will Update

--- FantasyRTSUnity/Assets/Scripts/Buildings/TestScript.cs
+++ FantasyRTSUnity/Assets/Scripts/Buildings/TestScript.cs
@@ -1,15 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TestScript : MonoBehaviour {
+public class TestScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //Editied by Will.
 }
