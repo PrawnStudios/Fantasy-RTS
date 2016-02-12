a9957805de79649ca0360c19813a2c77a90184a2
wmitch96 <w.mitch96@gmail.com>
2016-02-12 04:23:52 +0000

    Backup for Will

--- FantasyRTSUnity/Assets/Scripts/Account/Friends.cs
+++ FantasyRTSUnity/Assets/Scripts/Account/Friends.cs
@@ -16,16 +16,21 @@ public class Friends : MonoBehaviour
     private string removeFriendUrl = "http://prawnstudios.com/ingame/removefriend.php";
     private string getFriendsLIstUrl = "http://prawnstudios.com/ingame/getfriendslist.php";
 
-    public string[] friendsList = new string[0];
+    public bool friendOptions = false;
+    public Vector2 friendOptionsPos;
+
+    private string[] friendsList = new string[0];
+    public string selectedFriend;
 
     void Start()
     {
-        InvokeRepeating("UpdateFriendsList", 0, 5);
+        StartCoroutine("GetFriendList");
+        InvokeRepeating("UpdateFriendsList", 5, 5);
     }
 
     void UpdateFriendsList()
     {
-        if (friendButtonText == "Hide")
+        if (friendButtonText == "Hide" && friendsList.Length > 0)
         {
             StartCoroutine("GetFriendList");
         }
@@ -59,6 +64,10 @@ public class Friends : MonoBehaviour
             {
                 Debug.Log("User already Added");
             }
+            else if(logText == "No Friends Added")
+            {
+                Debug.Log("No Friends Added");
+            }
             else
             {
                 friendsList = logText.Split(':');
@@ -82,12 +91,11 @@ public class Friends : MonoBehaviour
 
         if (LoginAccountWWW.error != null)
         {
-            Debug.LogError("Cannot connect to login server.");
+            Debug.LogError("Cannot connect to server.");
         }
         else
         {            
             string logText = LoginAccountWWW.text;
-            Debug.Log(logText);
             if (logText == "Cannot find user")
             {
                 Debug.Log("Could not find the user to add.");
@@ -100,9 +108,32 @@ public class Friends : MonoBehaviour
         }
     }
 
-    void RemoveFriend()
+    IEnumerator RemoveFriend()
     {
-        
+        Debug.Log("Add Friend Called");
+
+        WWWForm form = new WWWForm(); //this is what sends messages to our php script
+
+        //the fields are the variables we are sending
+        form.AddField("FriendUsername", selectedFriend);
+        form.AddField("ID", PlayerPrefs.GetString("UserID"));
+
+        WWW LoginAccountWWW = new WWW(removeFriendUrl, form);
+        yield return LoginAccountWWW;
+
+        if (LoginAccountWWW.error != null)
+        {
+            Debug.LogError("Cannot connect to server.");
+        }
+        else
+        {
+            string logText = LoginAccountWWW.text;
+            if(logText == "Friend Removed")
+            {
+                Debug.Log("Removed Friend");
+                StartCoroutine("GetFriendList");
+            }
+        }
     }
 
     void OnGUI()
@@ -122,34 +153,59 @@ public class Friends : MonoBehaviour
         if (friendButtonText == "Hide")
         {
             GUI.Box(friendsListPos, "");
-            scrollPos = GUI.BeginScrollView(new Rect(Screen.width - 250, 35, 250, 350), scrollPos, new Rect(-10, 0, 230, friendsList.Length * 35));
-            //
-            int buttonY = 0;
-            foreach (string friend in friendsList)
+            if (friendsList.Length == 0)
             {
-                if (GUI.Button(new Rect(0, buttonY, 220, 25), friend)) //TODO add freinds status
-                {
-                    //TODO options when clicking freind
-                }
-                buttonY += 35;
+                GUI.Label(new Rect(Screen.width - 250, 35, 250, 350), "No Friends Added!");
             }
-            //
-            GUI.EndScrollView();
-            if(GUI.Button(new Rect(friendsListPos.x + 5, 395, 240, 25), "Add Friend"))
+            else
+            {
+                scrollPos = GUI.BeginScrollView(new Rect(Screen.width - 250, 35, 250, 350), scrollPos, new Rect(-10, 0, 230, friendsList.Length * 35));
+                //
+                int buttonY = 0;
+                foreach (string friend in friendsList)
+                {
+                    if (GUI.Button(new Rect(0, buttonY, 220, 25), friend))
+                    {
+                        if(Event.current.button == 0) // If Left CLick
+                        {
+                            //TODO View Profile
+                        }
+                        else if (Event.current.button == 1) // If Right Click
+                        {
+                            friendOptions = !friendOptions;
+                            friendOptionsPos = new Vector2(Screen.width - 400, buttonY + 35);
+                            selectedFriend = friend;
+                        }
+                    }
+                    buttonY += 35;
+                }
+                //
+                GUI.EndScrollView();               
+            }
+            if (GUI.Button(new Rect(friendsListPos.x + 5, 395, 240, 25), "Add Friend"))
             {
                 addFriend = !addFriend;
                 friendName = "Add Friend";
             }
-
-            if(addFriend)
+            if (addFriend)
             {
                 friendName = GUI.TextArea(new Rect(friendsListPos.x, 425, 250, 25), friendName);
-                if(GUI.Button(new Rect(friendsListPos.x, 450, 250, 25), "Confirm"))
+                if (GUI.Button(new Rect(friendsListPos.x, 450, 250, 25), "Confirm"))
                 {
-                    if(friendName != "")
+                    if (friendName != "" && friendName != "Add Friend")
                     {
                         StartCoroutine("AddFriend");
+                        friendName = "Add Friend";
+                        addFriend = false;
                     }
+                }
+            }
+
+            if(friendOptions)
+            {
+                if(GUI.Button(new Rect(friendOptionsPos.x, friendOptionsPos.y, 150, 25), "Remove Friend"))
+                {
+                    RemoveFriend();
                 }
             }
         }
